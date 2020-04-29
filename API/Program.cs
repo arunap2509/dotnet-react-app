12a9ad6d26b8ddd29a00d5e126531111f51dac7b
Arun A P <arundhas7@gmail.com>
2020-04-29 21:19:01 +0530

    Added CRUD operation

--- API/Program.cs
+++ API/Program.cs
@@ -26,7 +26,7 @@ namespace API
                 {
                     var context = service.GetRequiredService<DataContext>();
                     context.Database.Migrate();
-
+                    Seed.SeedData(context);
                 } 
                 catch (Exception ex) 
                 {
--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -12,6 +12,7 @@ namespace Persistance
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
